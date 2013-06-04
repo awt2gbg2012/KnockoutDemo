674b277e55b02f0da8a30cda8346931f263d2f6b
David Gullmarsvik <david.gullmarsvik@gmail.com>
2013-06-04 07:37:38 +0200

    Exercise Setup

---
+++ KnockoutDemo/Controllers/UserDetailsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using KnockoutDemo.Models;
+
+namespace KnockoutDemo.Controllers
+{
+    public class UserDetailsController : Controller
+    {
+        //
+        // GET: /Person/
+
+        [HttpPost]
+        public JsonResult Save(UserDetailsModel userDetails)
+        {
+            // Faking Logic to save person to DB
+            var message = string.Format("Saved {0} {1} {2}", userDetails.firstName, userDetails.lastName, userDetails.email);
+            message += string.Format(" with {0} friends", userDetails.friends.Count);
+            message += string.Format(" ({0} on twitter)", userDetails.friends.Where(s => s.isOnTwitter).Count());
+            return Json(new { message });
+        }
+
+        [HttpGet]
+        public JsonResult GetJSON()
+        {
+            // Logic to fetch person from DB
+            var UserDetails = new UserDetailsModel
+            {
+                firstName = "BertS",
+                lastName = "Smith",
+                email = "bertie@smith.com",
+                friends = new List<Friend> {
+                                new Friend { 
+                                    name = "SteveS", 
+                                    isOnTwitter = false
+                                }, 
+                                new Friend { 
+                                    name = "AnnieS", 
+                                    isOnTwitter = true, 
+                                    twitterHandle = "annie"
+                                }
+                            }
+            };
+            return Json(new JavaScriptSerializer().Serialize(UserDetails), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ KnockoutDemo/Models/UserDetailsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnockoutDemo.Models
+{
+    public class UserDetailsModel
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public List<Friend> friends { get; set; }
+    }
+
+    public class Friend
+    {
+        public string name { get; set; }
+        public bool isOnTwitter { get; set; }
+        public string twitterHandle { get; set; }
+    }
+}
